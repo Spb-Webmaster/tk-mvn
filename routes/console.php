40aef259305c78ccbe243b07196c2cefaac13a85
Axeld <spb-webmaster@mail.ru>
2026-06-06 13:41:59 +0300

    06-iyn-2026-2

--- routes/console.php
+++ routes/console.php
@@ -11,7 +11,7 @@ Schedule::command('queue:work --stop-when-empty')
     ->everyMinute()
     ->withoutOverlapping();
 
-Schedule::command('sitemap:cron')->dailyAt('03:00');
+Schedule::command('sitemap:cron')->everyMinute();
 
 Schedule::command('training:clear-dates')->dailyAt('00:10');
 //Schedule::command('training:clear-dates')->everyMinute();
